da59a2e12ec19940c895f871315e0280318022b3
Mert Simsek <mertsmsk0@gmail.com>
2023-08-01 14:18:12 +0300

    Update composer file and delete line of deleting the migration files

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,11 +5,10 @@ sleep 25
 cd /var/www/app
 
 rm -rf var/cache/*
-rm -rf migrations/Version*
 rm -rf var/log/messenger*
 
 php bin/console doctrine:database:create --if-not-exists
-php bin/console make:migration --no-interaction
+php bin/console doctrine:migrations:diff --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
 
 php bin/console --env=test doctrine:database:create --if-not-exists
